3c47ff75ae4327536f0491b0d5a5abcd47ce855c
asterox <62700841+roxov@users.noreply.github.com>
2020-04-19 14:10:55 +0200

    utf 8 et unix

--- src/fr/asterox/feteforaine/core/Player.java
+++ src/fr/asterox/feteforaine/core/Player.java
@@ -8,6 +8,9 @@ public class Player {
 	// Tant qu'aucune valeur n'est assignée à l'attribut, name vaut null.
 	private String name;
 	private LocalDateTime creationDate;
+	private int strength = 5;
+	private int courage = 5;
+	private int agility = 8;
 
 	// Nous avons maintenant un magnifique POJO.
 	public String getName() {
@@ -26,4 +29,30 @@ public class Player {
 		this.creationDate = creationDate;
 	}
 
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getCourage() {
+		return courage;
+	}
+
+	public void setCourage(int courage) {
+		this.courage = courage;
+	}
+
+	public int getAgility() {
+		return agility;
+	}
+
+	public void setAgility(int agility) {
+		this.agility = agility;
+	}
+
+		
+	
 }
--- tests/fr/asterox/feteforaine/buvette/OrderTest.java
+++ tests/fr/asterox/feteforaine/buvette/OrderTest.java
@@ -20,5 +20,41 @@ class OrderTest {
 	final void testDisplaySelectedMenu() {
 		fail("Not yet implemented");
 	}*/
+	/*import static org.junit.jupiter.api.Assertions.*;
 
+	import java.io.ByteArrayOutputStream;
+	import java.io.PrintStream;
+
+	import org.junit.jupiter.api.AfterEach;
+	import org.junit.jupiter.api.BeforeEach;
+	import org.junit.jupiter.api.Test;
+
+	public class OrderTest {
+
+		private final ByteArrayOutputStream outContent = new ByteArrayOutputStream () ;
+		
+		@BeforeEach
+		public void setUpStreams() {
+			System.setOut(new PrintStream(outContent));
+		}
+		
+		@AfterEach
+		public void restoreStreams() {
+			System.setOut(System.out);
+		}
+
+		Order order = new Order ();
+		
+		@Test
+		public void Given_Nothing_When_DisplayMenuSelection_Then_ShouldDisplayText() {
+			order.displayAvailableMenus();
+			assertEquals(false, outContent.toString().isEmpty());
+		}
+		
+		//public void Given_Vegetarian_When_AskForMenuSelected_Then_ShouldDisplayCorrectSentence() {
+			//order.displaySelectedMenu(3);
+			//assertEquals("Vous avez choisi le menu 3\n", outContent.toString().replace("\r\n","\n"));
+		//}
+		
+	}*/
 }
